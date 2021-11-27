193bd91d812017e9dcb618c535bf97559ecf2f64
Mike <MikeKonvalinka@gmail.com>
2021-11-26 23:37:50 -0600

    .

--- pages/api/about.js
+++ pages/api/about.js
@@ -1,9 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import from below is not correctly, probably due to where the file is being called from
 import styles from '~/styles/Home.module.css'
 
 export default function About() {
     return (
       <div>
+
          <h1 className={styles.title}>
            Testing About Page
           </h1>
