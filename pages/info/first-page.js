4bf4f8f46c609617bf5a29f1246f9ad614a4b031
Mike <MikeKonvalinka@gmail.com>
2021-12-18 23:57:06 -0600

    updated css pathing

--- pages/info/first-page.js
+++ pages/info/first-page.js
@@ -1,4 +1,5 @@
 // when creating page in next.js need to 'export default function codehere(){}'
+import styles from '../../styles/Index.css'
 
 export default function About(){
     return <h1> test </h1>
